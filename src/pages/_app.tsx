5f733aa3b2925af2d2e3964e603b5f22b9178aec
minguk-jang <mgjang0706@gmail.com>
2025-04-25 09:58:36 +0900

    mod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,3 +4,9 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+import Script from "next/script";
+
+<Script
+  src="https://developers.kakao.com/sdk/js/kakao.js"
+  strategy="beforeInteractive"
+/>
