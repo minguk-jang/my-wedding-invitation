9e932c2e4eda96796bac1eb0a2933282186c3dbb
minguk-jang <mgjang0706@gmail.com>
2025-04-27 19:16:36 +0900

    mod

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -245,8 +245,8 @@ export default function Location() {
           </NaviButton>
         </ButtonGroup> */}
         <ButtonGroup>
-          <NaviButton
-            href="https://map.kakao.com/link/map/아펠가모%20반포,37.5008416,127.0032287"
+        <NaviButton
+            href="https://navigate.kakao.com/launch?name=아펠가모반포&x=127.0032287&y=37.5008416"
             target="_blank"
           >
             <NaviIcon src="/images/kakao_navi.svg" alt="카카오내비 아이콘" />
