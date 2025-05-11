c251976578501ee2581ba5cc6797b2bc71ca2cb2
minguk-jang <mgjang0706@gmail.com>
2025-05-11 17:22:50 +0900

    mod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,32 @@
-// import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
+// // pages/_app.tsx
+// import '@/styles/globals.css';
+// import type { AppProps } from 'next/app';
+// import { applyGlobalFont } from '@/styles/globalFont'; // ✅ 추가
+// applyGlobalFont(); // ✅ 호출
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { applyGlobalFont } from '@/styles/globalFont'; // ✅ 추가
+import { useEffect } from "react";
 
+applyGlobalFont(); // ✅ 호출
 
 // export default function App({ Component, pageProps }: AppProps) {
 //   return <Component {...pageProps} />;
 // }
-// import Script from "next/script";
-
-// <Script
-//   src="https://developers.kakao.com/sdk/js/kakao.js"
-//   strategy="beforeInteractive"
-// />
-
-// import Head from "next/head";
-
-// <Head>
-//   <link
-//     href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400&display=swap"
-//     rel="stylesheet"
-//   />
-// </Head>
-
-// pages/_app.tsx
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { applyGlobalFont } from '@/styles/globalFont'; // ✅ 추가
-
-applyGlobalFont(); // ✅ 호출
-
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      if (e.ctrlKey) {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("wheel", handleWheel, { passive: false });
+    return () => window.removeEventListener("wheel", handleWheel);
+  }, []);
+
   return <Component {...pageProps} />;
 }
