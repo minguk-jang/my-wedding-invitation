151504147a99f9352f6609e40c80163eaf7fdd9d
minguk-jang <mgjang0706@gmail.com>
2025-06-07 17:16:49 +0900

    Updates

---
+++ _next/static/E6vmfsQMbz1gw6zFi3UmW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d1600f0b19d60dbb.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
