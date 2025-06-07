bbcc36afeb166defdd5449d1d4fbaa0c0bc5d3ff
minguk-jang <mgjang0706@gmail.com>
2025-06-07 18:26:06 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,7 +86,7 @@ export default function Home() {
               }}
               initial={{ opacity: 0, y: 50 }}
               whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true, amount: 0.2 }}
+              viewport={{ once: true, amount: 0.5 }}
               transition={{ duration: 1.2 }}
             >
               <img 
