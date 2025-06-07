aa2f49a63a30b3eb3581f2dfc9759f951f23c128
minguk-jang <mgjang0706@gmail.com>
2025-06-07 18:25:19 +0900

    Updates

--- _next/static/9zF85Ig4x_V6nm7tTPWsV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/16-763cad8059a1d11e.js","static/chunks/pages/index-10376397598d920f.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/EWb_Q1QxaUUuRuWcYu0_X/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/16-763cad8059a1d11e.js","static/chunks/pages/index-128ce04760bffe4f.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
